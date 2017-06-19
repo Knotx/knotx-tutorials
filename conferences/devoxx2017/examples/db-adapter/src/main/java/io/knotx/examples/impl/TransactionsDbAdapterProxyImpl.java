7ca305675bb0d89dc82b772648c53fdda9a74c28
Maciej Laskowski <skejven@gmail.com>
2017-06-19 14:56:50 +0200

    setup demo modules

---
+++ conferences/devoxx2017/examples/db-adapter/src/main/java/io/knotx/examples/impl/TransactionsDbAdapterProxyImpl.java
@@ -0,0 +1,48 @@
+package io.knotx.examples.impl;
+
+import io.knotx.adapter.AbstractAdapterProxy;
+import io.knotx.dataobjects.AdapterRequest;
+import io.knotx.dataobjects.AdapterResponse;
+import io.knotx.dataobjects.ClientResponse;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.rxjava.ext.jdbc.JDBCClient;
+import rx.Single;
+
+public class TransactionsDbAdapterProxyImpl extends AbstractAdapterProxy {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TransactionsDbAdapterProxyImpl.class);
+  private final JDBCClient client;
+
+  public TransactionsDbAdapterProxyImpl(JDBCClient client) {
+    this.client = client;
+  }
+
+  @Override
+  protected Single<AdapterResponse> processRequest(AdapterRequest adapterRequest) {
+    final String query = adapterRequest.getParams().getString("query");
+    LOGGER.info("Processing request with query: `{}`", query);
+
+//   TODO: client.rxGetConnection()
+    return null;
+
+//    return client.getConnectionObservable()
+//        .flatMap(
+//            sqlConnection -> sqlConnection.queryObservable(query)
+//        )
+//        .doOnNext(rs -> rs.getRows().forEach(LOGGER::info))
+//        .map(this::toAdapterResponse);
+  }
+
+  private AdapterResponse toAdapterResponse(ResultSet rs) {
+    final AdapterResponse adapterResponse = new AdapterResponse();
+    final ClientResponse clientResponse = new ClientResponse();
+    clientResponse.setBody(Buffer.buffer(new JsonArray(rs.getRows()).encode()));
+    adapterResponse.setResponse(clientResponse);
+    return adapterResponse;
+  }
+
+}
