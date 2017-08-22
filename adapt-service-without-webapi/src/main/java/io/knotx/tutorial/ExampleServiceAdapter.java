3aacd6cdf8e530315993c597b04c238464e43184
dariusz.blicharski <dariusz.blicharski@cognifide.com>
2017-08-22 12:01:48 +0200

    cr fixes. Added untracked files

---
+++ adapt-service-without-webapi/src/main/java/io/knotx/tutorial/ExampleServiceAdapter.java
@@ -0,0 +1,65 @@
+/*
+    Copyright (C) 2016 Cognifide Limited
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package io.knotx.tutorial;
+
+import io.knotx.proxy.AdapterProxy;
+import io.vertx.core.Context;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.ext.jdbc.JDBCClient;
+import io.vertx.serviceproxy.ProxyHelper;
+
+public class ExampleServiceAdapter extends AbstractVerticle {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ExampleServiceAdapter.class);
+
+  private MessageConsumer<JsonObject> consumer;
+  private ExampleServiceAdapterConfiguration configuration;
+
+  @Override
+  public void init(Vertx vertx, Context context) {
+    LOGGER.debug("Starting <{}>", this.getClass().getSimpleName());
+    super.init(vertx, context);
+    // using config() method from AbstractVerticle we simply pass our JSON file configuration to Java model
+    configuration = new ExampleServiceAdapterConfiguration(config());
+  }
+
+  @Override
+  public void start() throws Exception {
+    LOGGER.debug("Starting <{}>", this.getClass().getSimpleName());
+    //create JDBC Clinet here and pass it to AdapterProxy - notice using clientOptions property here
+    final JDBCClient client = JDBCClient.createShared(vertx, configuration.getClientOptions());
+
+    //register the service proxy on the event bus, notice using `getVertx()` here to obtain non-rx version of vertx
+    consumer = ProxyHelper
+        .registerService(AdapterProxy.class, getVertx(),
+            new ExampleServiceAdapterProxy(client),
+            configuration.getAddress());
+  }
+
+  @Override
+  public void stop() throws Exception {
+    LOGGER.debug("Starting <{}>", this.getClass().getSimpleName());
+    // unregister adapter when no longer needed
+    ProxyHelper.unregisterService(consumer);
+  }
+
+}
---
+++ adapt-service-without-webapi/src/main/java/io/knotx/tutorial/ExampleServiceAdapterProxy.java
@@ -0,0 +1,61 @@
+/*
+    Copyright (C) 2016 Cognifide Limited
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package io.knotx.tutorial;
+
+import io.knotx.adapter.AbstractAdapterProxy;
+import io.knotx.dataobjects.AdapterRequest;
+import io.knotx.dataobjects.AdapterResponse;
+import io.knotx.dataobjects.ClientResponse;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.rxjava.ext.jdbc.JDBCClient;
+import rx.Single;
+
+public class ExampleServiceAdapterProxy extends AbstractAdapterProxy {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ExampleServiceAdapterProxy.class);
+
+  //we will need JDBC Client here to perform DB queries
+  private final JDBCClient client;
+
+  public ExampleServiceAdapterProxy(JDBCClient client) {
+    this.client = client;
+  }
+
+  @Override
+  protected Single<AdapterResponse> processRequest(AdapterRequest adapterRequest) {
+    final String query = adapterRequest.getParams().getString("query");
+    LOGGER.info("Processing request with query: `{}`", query);
+    return client.rxGetConnection()
+        .flatMap(
+            sqlConnection -> sqlConnection.rxQuery(query)
+        )
+        .map(this::toAdapterResponse);
+  }
+
+  private AdapterResponse toAdapterResponse(ResultSet rs) {
+    final AdapterResponse adapterResponse = new AdapterResponse();
+    final ClientResponse clientResponse = new ClientResponse();
+    clientResponse.setBody(Buffer.buffer(new JsonArray(rs.getRows()).encode()));
+    adapterResponse.setResponse(clientResponse);
+    return adapterResponse;
+  }
+
+}
